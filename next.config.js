3410b96bb99de69a005beb161f2d8b6fa2fd0005
Akarsh Gupta <22f3002362@ds.study.iitm.ac.in>
2025-07-31 10:52:19 +0530

    optimized the code

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // output: 'export',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    formats: ['image/webp']
+  }
+};
+
+module.exports = nextConfig;
